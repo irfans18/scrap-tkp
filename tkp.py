de7ca91e962848c7be20a28024a13d84ab37178c
irfans18 <58197653+irfans18@users.noreply.github.com>
2022-03-18 18:57:33 +0700

    add attribute
    seller, url

--- tkp.py
+++ tkp.py
@@ -26,7 +26,7 @@ def query_search(driver, search_term):
 
 
 def save_tweet_data_to_csv(records, filepath, mode='a+'):
-   header = ['titles', 'prices', 'rates']
+   header = ['Subjek', 'Harga', 'Rating', 'Penjual', 'Asal', 'url']
    with open(filepath, mode=mode, newline='', encoding='utf-8') as f:
       writer = csv.writer(f, delimiter='\t')
       if mode == 'w':
@@ -55,24 +55,11 @@ def load_catalog(driver):
       y += 600
       sleep(2)
 
-   # titles, prices, rates = [], [], []
-   # data = pd.DataFrame()
-
-   # catalog = driver.find_element(
-   #    By.XPATH, '//div[@data-testid="divSRPContentProducts"]')
-   # container = catalog.find_elements(
-   #    By.XPATH, './/div[@class="css-jza1fo"]')
    cards = driver.find_elements(
        By.XPATH, './/div[@class="css-12sieg3"]')
    return cards
-   # print("Total container : ", len(container))
-   # print("Total cards : ", len(cards))
-   # driver.find_element(
-   #    By.XPATH, '//div[@class="unf-coachmark__next-button css-64apm5 e1o9jid35"]').click()
 
 
-# count = 0
-# for card in cards:
 def extract_from_card(card):
    title = card.find_element(
        By.XPATH, './/div[@class="css-1b6t4dn"]').text
@@ -83,48 +70,45 @@ def extract_from_card(card):
           By.XPATH, './/span[@class="css-t70v7i"]').text
    except NoSuchElementException:
       rate = "0"
-   # count += 1
-   product = (title, price, rate)
+   sellers = card.find_elements_by_class_name('css-1kdc32b')
+   count = 0
+   for seller in sellers:
+      if count == 0:
+         seller_town = seller.text
+      else:
+         seller_user = seller.text
+      count += 1
+   seller_url = card.find_element(
+       By.XPATH, './/div[@class="css-zimbi"]/a').get_attribute('href')
+   product = (title, price, rate, seller_user, seller_town, seller_url)
    return product
-
-   # titles.append(title)
-   # prices.append(price)
-   # rates.append(rate)
-   # print(count, "\t", titles, "\t", prices, "\t", rates, "\n")
-   # print(count, "\t", titles, "\t", prices, "\n")
-# sleep(10)
-
-# data['titles'] = titles
-# data['prices'] = prices
-# data['rates'] = rates
-# print(data)
 
 
 def main(search_term, pages, filepath):
    driver = create_webdriver_instance()
    save_tweet_data_to_csv(None, filepath, 'w')
    query_search(driver, search_term)
+   count = 0
    for i in range(pages):
       if i == 0:
          driver.find_element(
              By.XPATH, '//div[@class="unf-coachmark__next-button css-64apm5 e1o9jid35"]').click()
 
       cards = load_catalog(driver)
-      count = 0
       for card in cards:
          product = extract_from_card(card)
          save_tweet_data_to_csv(product, filepath)
          count += 1
-         print(count)
+         print((i+1), "-", count)
 
-      # driver.find_element(
-      #     By.XPATH, "//button[@aria-label='Halaman berikutnya']").click()
+      if i == pages:
+         break
       find_next_button(driver)
 
 
 if __name__ == '__main__':
-   term = 'iphone'
+   term = input("Search Term : ")
    path = term + '.txt'
-   pages = 4
+   pages = 2
 
    main(term, pages, path)
